20a85125d35e1b03e5b7b61ef71191f08e1c2c62
Gem <gem@ladyprogrammer.net>
2023-03-12 21:00:27 +0800

    refactor scss

--- src/app/thank-you/thank-you.component.ts
+++ src/app/thank-you/thank-you.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-thank-you',
   templateUrl: './thank-you.component.html',
-  styleUrls: ['../dialog.component.scss', './thank-you.component.scss']
+  styleUrls: ['./thank-you.component.scss']
 })
 export class ThankYouComponent {
   @Input() selectedRating!: number;
